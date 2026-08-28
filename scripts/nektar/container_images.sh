7ffd220c5744471d3bf4843e2aef519b70d52f17
hwustenb <hwustenb@ic.ac.uk>
2026-08-28 15:05:28 +0100

    Update nektar image to latest

--- scripts/nektar/container_images.sh
+++ scripts/nektar/container_images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Immutable full Nektar++ OCI image used by every Nektar++ executable.
 
-NEKTAR_RELEASE_DEFAULT="v5.10.0"
-NEKTAR_IMAGE_DEFAULT="nektarpp/nektar@sha256:2ae26f90b902742b7b2a7e6c9a18542b171e654a26f54b9944ab636d24da3748"
+NEKTAR_RELEASE_DEFAULT="latest"
+NEKTAR_IMAGE_DEFAULT="nektarpp/nektar"
